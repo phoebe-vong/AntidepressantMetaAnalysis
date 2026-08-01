d11ce09707ff767e00d48f665bfcd967d0256d8f
phoebe-vong <phoebecvong@gmail.com>
2026-08-01 11:09:18 -0400

    Add files via upload
    BDA 2026 Code Session Info

---
+++ BDA_Antidepressant_SessionInfo_2026.R
@@ -0,0 +1,39 @@
+#Antidepressant Team - R Session Info
+#Phoebe Vong
+#July 31 2026
+
+
+#R version 4.6.0 (2026-04-24 ucrt)
+#Platform: x86_64-w64-mingw32/x64
+#Running under: Windows 11 x64 (build 26200)
+
+#Matrix products: default
+#LAPACK version 3.12.1
+
+#locale:
+#  [1] LC_COLLATE=English_United States.utf8  LC_CTYPE=English_United States.utf8   
+#[3] LC_MONETARY=English_United States.utf8 LC_NUMERIC=C                          
+#[5] LC_TIME=English_United States.utf8    
+
+#time zone: America/New_York
+#tzcode source: internal
+
+#attached base packages:
+#  [1] stats     graphics  grDevices utils     datasets  methods   base     
+
+#other attached packages:
+#  [1] dichromat_2.0-1     pheatmap_1.0.13     multtest_2.68.0     Biobase_2.72.0      BiocGenerics_0.58.1
+#[6] generics_0.1.4      metafor_5.0-1       numDeriv_2016.8-1.1 metadat_1.6-0       Matrix_1.7-5       
+#[11] plyr_1.8.9          lubridate_1.9.5     forcats_1.0.1       stringr_1.6.0       dplyr_1.2.1        
+#[16] purrr_1.2.2         readr_2.2.0         tidyr_1.3.2         tibble_3.3.1        ggplot2_4.0.3      
+#[21] tidyverse_2.0.0     BiocManager_1.30.27 gemma.R_3.8.0      
+
+#loaded via a namespace (and not attached):
+#  [1] stringi_1.8.7      lattice_0.22-9     hms_1.1.4          digest_0.6.39      magrittr_2.0.5     grid_4.6.0        
+#[7] timechange_0.4.0   RColorBrewer_1.1-3 survival_3.8-6     httr_1.4.8         scales_1.4.0       cli_3.6.6         
+#[13] rlang_1.2.0        splines_4.6.0      bit64_4.8.2        withr_3.0.3        tools_4.6.0        tzdb_0.5.0        
+#[19] mathjaxr_2.0-0     curl_7.1.0         vctrs_0.7.3        R6_2.6.1           stats4_4.6.0       lifecycle_1.0.5   
+#[25] bit_4.6.0          MASS_7.3-65        pkgconfig_2.0.3    pillar_1.11.1      gtable_0.3.6       data.table_1.18.4 
+#[31] glue_1.8.1         Rcpp_1.1.2         tidyselect_1.2.1   rstudioapi_0.19.0  farver_2.1.2       nlme_3.1-169      
+#[37] compiler_4.6.0     S7_0.2.2  
+
